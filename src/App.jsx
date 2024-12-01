430cc6955414b5e7ccff3ea8c137c6fa0c561398
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-12-01 16:11:13 +0300

    refactoring App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import {Layout} from 'antd';
+import {AppHeader} from "./components/layout/AppHeader.jsx";
+import {AppSider} from "./components/layout/AppSider.jsx";
+import {AppContent} from "./components/layout/AppContent.jsx";
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+  return (
+      <Layout>
+        <AppHeader />
+        <Layout>
+          <AppSider />
+          <AppContent />
+        </Layout>
+      </Layout>
+  )
 }
