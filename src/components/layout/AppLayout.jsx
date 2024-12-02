c47863d5779f5c4cf9ded4a5f298672480eb8a4b
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-12-02 22:17:19 +0300

    refactoring AppLayout component

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -2,12 +2,11 @@ import {AppHeader} from "./AppHeader.jsx";
 import {Layout, Spin} from "antd";
 import {AppSider} from "./AppSider.jsx";
 import {AppContent} from "./AppContent.jsx";
-import {useContext} from "react";
-import {CryptoContext} from "../../context/crypto-context.jsx";
+import {useCrypto} from "../../context/crypto-context.jsx";
 
 export const AppLayout = () => {
 
-    const {loading} = useContext(CryptoContext)
+    const {loading} = useCrypto()
 
     if (loading) {
         return <Spin fullscreen />
